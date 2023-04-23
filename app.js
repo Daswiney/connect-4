05a93b0bcf29f2e3ec5b53d053c68b8c6a5b8594
Daswiney <daswiney@yahoo.com>
2023-04-23 19:10:27 -0400

    Updated version
    changes styles in css,
    added winning chip highlight,
    removed unneeded code,
    added move timer<
    and functions to make a random move for player>

--- app.js
+++ app.js
@@ -51,42 +51,48 @@ for (let i = 0; i < 42; i++) {
 }
 
 function onColumnClick(column) {
-    let openRow = pieces.filter( (_, index) => index % 7 === column).lastIndexOf(0);
-    if(openRow === -1){
-        //no space in column
-        return;
+    startTimer();
+    let openRow = pieces.filter((_, index) => index % 7 === column).lastIndexOf(0);
+    if (openRow === -1) {
+      //no space in column
+      return;
     }
     pieces[(openRow * 7) + column] = playerTurn;
     let cell = board.children[(openRow * 7) + column];
-
+  
     let piece = document.createElement('div');
     piece.className = 'piece';
     piece.dataset.player = playerTurn;
     piece.dataset.placed = true;
     cell.appendChild(piece);
-
+  
     let unplaced = document.querySelector("[data-placed='false']");
-    let unplacedY = unplaced.getBoundingClientRect().y;
-    let placedY = piece.getBoundingClientRect().y;
-    let yDiff = unplacedY - placedY;
-
-    animating = true;
-    removeUnplaced();
-    let animation = piece.animate(
+    if (unplaced) { //without this makeRandomMove() can bug out -- just skips animation if hover piece not found
+      let unplacedY = unplaced.getBoundingClientRect().y;
+      let placedY = piece.getBoundingClientRect().y;
+      let yDiff = unplacedY - placedY;
+  
+      animating = true;
+      removeUnplaced();
+      let animation = piece.animate(
         [
-            { transform: `translateY(${yDiff}px)`, offset: 0},
-            { transform: `translateY(0px)`, offset: .5},
-            { transform: `translateY(${yDiff / 20}px)`, offset: .75},
-            { transform: `translateY(0px)`, offset: 1}
+          { transform: `translateY(${yDiff}px)`, offset: 0 },
+          { transform: `translateY(0px)`, offset: 0.5 },
+          { transform: `translateY(${yDiff / 20}px)`, offset: 0.75 },
+          { transform: `translateY(0px)`, offset: 1 },
         ],
         {
-            duration: 400,
-            easing: "linear",
-            iterations: 1
+          duration: 400,
+          easing: 'linear',
+          iterations: 1,
         }
-    )
-    animation.addEventListener('finish', checkGameEnd)
-}
+      );
+      animation.addEventListener('finish', checkGameEnd);
+    } else {
+      checkGameEnd();
+    }
+  }
+  
 
 function checkGameEnd() {
     animating = false;    
@@ -143,28 +149,38 @@ function onMouseEnterColumn(column) {
 
 function playerWin(playerTurn, pieces){
     for (let index = 0; index < 42; index++) {
-        //Check horizontal win at starting index
         if(
+        //Check horizontal win at starting index
             index % 7 < 4 &&
             pieces[index] === playerTurn &&
             pieces[index + 1] === playerTurn &&
             pieces[index + 2] === playerTurn &&
             pieces[index + 3] === playerTurn 
             ) {
+                board.children[index].firstChild.dataset.highlighted = true; // highlight winning pieces
+                board.children[index + 1].firstChild.dataset.highlighted = true;
+                board.children[index + 2].firstChild.dataset.highlighted = true;
+                board.children[index + 3].firstChild.dataset.highlighted = true;
+                clearInterval(intervalId); // reset timer so pieces dont drop after game end
                 return true;
-        }
-        //Check vertical win at starting index
+            }
         if(
+        //Check vertical win at starting index
             index < 21 &&
             pieces[index] === playerTurn &&
             pieces[index + 7] === playerTurn &&
             pieces[index + 14] === playerTurn &&
             pieces[index + 21] === playerTurn 
             ) {
+                board.children[index].firstChild.dataset.highlighted = true;
+                board.children[index + 7].firstChild.dataset.highlighted = true;
+                board.children[index + 14].firstChild.dataset.highlighted = true;
+                board.children[index + 21].firstChild.dataset.highlighted = true;
+                clearInterval(intervalId);
                 return true;
-        }
-        //Check diagonal win (top left to bottom right) at starting index
+              }
         if(
+        //Check diagonal win (top left to bottom right) at starting index
             index % 7 < 4 &&
             index < 18 &&
             pieces[index] === playerTurn &&
@@ -172,10 +188,15 @@ function playerWin(playerTurn, pieces){
             pieces[index + 16] === playerTurn &&
             pieces[index + 24] === playerTurn 
             ) {
+                board.children[index].firstChild.dataset.highlighted = true;
+                board.children[index + 8].firstChild.dataset.highlighted = true;
+                board.children[index + 16].firstChild.dataset.highlighted = true;
+                board.children[index + 24].firstChild.dataset.highlighted = true;
+                clearInterval(intervalId);
                 return true;
-        }
-        //Check diagonal win (top right to bottom left) at starting index
+              }
         if(
+        //Check diagonal win (top right to bottom left) at starting index
             index % 7 >= 3 &&
             index < 21 &&
             pieces[index] === playerTurn &&
@@ -183,31 +204,55 @@ function playerWin(playerTurn, pieces){
             pieces[index + 12] === playerTurn &&
             pieces[index + 18] === playerTurn 
             ) {
+                board.children[index].firstChild.dataset.highlighted = true;
+                board.children[index + 6].firstChild.dataset.highlighted = true;
+                board.children[index + 12].firstChild.dataset.highlighted = true;
+                board.children[index + 18].firstChild.dataset.highlighted = true;
+                clearInterval(intervalId); 
                 return true;
-        }
-        //Check diagonal win (bottom left to top right) at starting index
-        if(
-            index % 7 < 4 &&
-            index >= 21 &&
-            pieces[index] === playerTurn &&
-            pieces[index - 6] === playerTurn &&
-            pieces[index - 12] === playerTurn &&
-            pieces[index - 18] === playerTurn 
-            ) {
-                return true;
-        }
-        //Check diagonal win (bottom right to top left) at starting index
-        if(
-            index % 7 >= 3 &&
-            index >= 21 &&
-            pieces[index] === playerTurn &&
-            pieces[index - 8] === playerTurn &&
-            pieces[index - 16] === playerTurn &&
-            pieces[index - 24] === playerTurn 
-            ) {
-                return true;
-        }
+              }
 
     }
     return false;
 }
+
+let intervalId = null; 
+const timerElement = document.getElementById("timer");
+timerElement.innerHTML = `Drop a Piece to Begin`;
+
+function startTimer() {
+  let seconds = 10;
+  let countdown = seconds;
+  timerElement.innerHTML = `auto move in: ${countdown} s`;
+  clearInterval(intervalId); // reset timer on each move
+  intervalId = setInterval(() => {
+    countdown--;
+    if (countdown >= 0) {
+      timerElement.innerHTML = `auto move in: ${countdown} s`;
+    } else {
+      clearInterval(intervalId);
+      makeRandomMove();
+    }
+  }, 1000);
+}
+
+  function makeRandomMove() { //grabs a random open column and simulates a player click
+    let validCols = getOpenCols();
+    if (validCols.length === 0) {
+      return;
+    }
+    let randomCol = validCols[Math.floor(Math.random() * validCols.length)];
+    onColumnClick(randomCol);
+  }
+  
+  function getOpenCols() { //makes array of open columns
+    let openCols = [];
+    for (let column = 0; column < 7; column++) {
+      let openCol = pieces.filter((_, index) => index % 7 === column).lastIndexOf(0);
+      if (openCol !== -1) {
+        openCols.push(column);
+      }
+    }
+    return openCols;
+  }
+  
